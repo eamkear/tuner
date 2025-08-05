92b762e1b5cf75f1b0196f27e83cf4db8c3552b3
eamkear <eamkear@yahoo.com>
2025-08-05 02:15:05 +0100

    Create tuning.js

---
+++ constants/tuning.js
@@ -0,0 +1,8 @@
+export const G_TUNING = [
+  { stringNum: 6, note: 'D2', freq: 73.42 },
+  { stringNum: 5, note: 'G2', freq: 98.00 },
+  { stringNum: 4, note: 'D3', freq: 146.83 },
+  { stringNum: 3, note: 'G3', freq: 196.00 },
+  { stringNum: 2, note: 'B3', freq: 246.94 },
+  { stringNum: 1, note: 'D4', freq: 293.66 },
+];
